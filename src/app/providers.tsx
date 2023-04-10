f3d5631eaae82c01bcd0df249dfbf0d5603716f1
Haardik <hhaardik@uwaterloo.ca>
2023-04-10 12:13:37 -0400

    Add @vercel/analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
-
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      {children}
+      <Analytics />
+    </ThemeProvider>
+  );
 }
